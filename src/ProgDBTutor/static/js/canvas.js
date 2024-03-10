a074b77fb8492c2995c829ab41f20e0c3b0fa555
Joren Van der Sande <joren.vandersande@gmail.com>
2024-03-10 22:51:22 +0100

    pre-loaden van terrain tiles png's werkt

--- src/ProgDBTutor/static/js/canvas.js
+++ src/ProgDBTutor/static/js/canvas.js
@@ -5,52 +5,43 @@ import { handleKeyDown } from './userInputHandler.js'
 const canvas = document.getElementById('terrainCanvas');
 const ctx = canvas.getContext('2d');
 const tileSize = 64;
-
-
 const mapData = generateRandomMap(40, 40);
-
 const terrainMap = new gameTerrainMap(mapData, tileSize, ctx);
-//terrainMap.fetchTiles();
-
-document.addEventListener('keydown', (event) => {
-    handleKeyDown(event, terrainMap);
-});
-
-
-
-/*
-function drawTiles() {
-    for (let j = 0; j < Math.ceil(window.innerHeight / tileSize); j++) {
-        for (let i = 0; i < Math.ceil(window.innerWidth / tileSize); i++) {
-            const img = new Image();
-            let randomInteger = Math.floor(Math.random() * 6) + 1;
-            if (randomInteger === 5) {
-                randomInteger = 3;
-            }
-            const filePath = "/static/img/assets/tiles/Grass/Grass.6." + randomInteger.toString() + ".png";
-            img.src = filePath;
-            img.onload = (function(x, y) {
-                return function() {
-                    ctx.drawImage(img, x * tileSize, y * tileSize, tileSize, tileSize);
-                };
-            })(i, j); // IIFE
-        }
-    }
-}
-*/
-
 
 function resizeCanvas() {
+    // todo zorgen dat viewX en viewY naar geldige waardes gaan
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     ctx.imageSmoothingEnabled = false;
-    terrainMap.drawTiles(); // Redraw tiles after resizing
+    terrainMap.drawTiles(); // Redraw terrain after resizing
 }
 
-// Initial resize and draw
-resizeCanvas();
 
-window.addEventListener('resize', resizeCanvas);
+// Game wereld initiäliseren
+terrainMap.initialize().then(() => {
+    resizeCanvas(); // Initial resize and draw
+    window.addEventListener('resize', resizeCanvas);
+    document.addEventListener('keydown', (event) => {
+        handleKeyDown(event, terrainMap);
+    });
+}).catch(error => {
+    console.error('Initialization failed:', error);
+    // todo load error page (error getekend met de map tiles)
+});
+
+
+//terrainMap.fetchTiles();
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- src/ProgDBTutor/static/js/gameClasses.js
+++ src/ProgDBTutor/static/js/gameClasses.js
@@ -2,8 +2,17 @@ function getAssetDir(assetName) {
     return assetName.split('.')[0];
 }
 
+// Debug
+const terrainAssetList =
+    { terrain:
+        {
+            Grass: ["Grass.1.1","Grass.1.2","Grass.1.3","Grass.2.1","Grass.2.2","Grass.2.3","Grass.3.1","Grass.3.2","Grass.3.3","Grass.6.1","Grass.6.2","Grass.6.3","Grass.6.4","Grass.6.5","Grass.6.6","Grass.7.1","Grass.7.2","Grass.7.3","Grass.7.4","Grass.7.5","Grass.7.6"],
+            Water: ["Water.1.1","Water.1.2","Water.1.3","Water.1.4"]
+        }
+    }
 
-/*  Tile coördinaten: (y,x)
+
+/** Tile coördinaten: (y,x)
     0 1 2 3 | x
     1
     2
@@ -22,49 +31,59 @@ export class gameTerrainMap {
 
         this.viewY = 0;
         this.viewX = 0;
+
+        this.terrainAssetList = {}; // Bevat de json van alle asset file names die ingeladen moeten worden
+        this.terrainAssets = {}; // Bevat {"pad_naar_asset": imageObject}
+
     }
 
-    isValidPosition(y, x) {
-        return y >= 0 && y < this.height
-            && x >= 0 && x < this.width;
+    async initialize() {
+        //this.fetchTerrainMapData();
+        //await this.fetchTerrainAssetList();
+        await new Promise((resolve) => this.preloadTerrainAssets(resolve));
+        // Safe to call stuff here
     }
 
-    getTile(y, x) {
-        if (this.isValidPosition(y, x)) {
-            return this.tiles[y][x];
-        } else {
-            console.error("gameTerrainMap : Invalid position: (y:", y, ", x:", x, ")");
-            return null;
-        }
+    fetchTerrainAssetList() {
+        return fetch('/api/get-asset-list')
+            .then(response => response.json())
+            .then(data => {
+                console.log(data); // DEBUG
+                this.terrainAssetList =  data.terrain;
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                throw error;
+            });
     }
 
-    drawTiles() {
-        const windowTileHeight = Math.ceil(window.innerHeight / this.tileSize);
-        const windowTileWidth = Math.ceil(window.innerWidth / this.tileSize);
+    preloadTerrainAssets(callback) {
+        let assetMap = {};
+        let assetList = terrainAssetList; // todo als fetch werkt veranderen door 'this.terrainAssetList'
+        let totalCount = 0;
+        let loadedCount = 0;
 
-        let y_screen = 0;
-        let x_screen = 0;
-
-        for (let i_map = this.viewY; i_map < windowTileHeight + this.viewY; i_map++) {
-            x_screen = 0;
-            for (let j_map = this.viewX; j_map < windowTileWidth + this.viewX; j_map++) {
-                const currTile = this.tiles[i_map][j_map];
-                const filePath = "/static/img/assets/tiles/" + getAssetDir(currTile) + "/" + currTile + ".png";
+        // Is een beetje ingewikkeld doordat image loading asynchronous is
+        for (const terrainType in assetList.terrain) {
+            totalCount += assetList.terrain[terrainType].length;
+            assetList.terrain[terrainType].forEach(asset => {
+                const currPath = "/static/img/assets/terrain/" + terrainType + "/" + asset + ".png";
                 const img = new Image();
-                img.src = filePath;
-                img.onload = ((y, x) => { // (i, j) capturen zodat deze niet veranderen tijdens uitvoering
-                    return () => {
-                        this.ctx.drawImage(img, x * this.tileSize, y * this.tileSize, this.tileSize, this.tileSize);
-                    };
-                })(y_screen, x_screen); // IIFE
-                x_screen += 1;
-            }
-            y_screen += 1;
+                img.src = currPath;
+                img.onload = () => {
+                    assetMap[currPath] = img;
+                    loadedCount++;
+                    if (loadedCount === totalCount) {
+                        this.terrainAssets = assetMap;
+                        callback(); // Als alle images geladen zijn -> callback
+                    }
+                };
+            });
         }
     }
 
     // Haalt de terrain_tiles uit de database en update deze klasse
-    fetchTiles() {
+    fetchTerrainMapData() {
         console.log("fetchTiles wordt opgeroepen...")
         fetch('/api/get-game-terrain-map')
             .then(response => response.json()) // Parse JSON response
@@ -78,6 +97,46 @@ export class gameTerrainMap {
         this.viewX = 0;
         this.viewY = 0;
         this.drawTiles();
+    }
+
+
+    drawTiles() {
+        const windowTileHeight = Math.ceil(window.innerHeight / this.tileSize);
+        const windowTileWidth = Math.ceil(window.innerWidth / this.tileSize);
+
+        for (let y_screen = 0, i_map = this.viewY; y_screen < windowTileHeight; y_screen++, i_map++) {
+            for (let x_screen = 0, j_map = this.viewX; x_screen < windowTileWidth; x_screen++, j_map++) {
+                let filePath;
+                if (this.tiles[i_map] && this.tiles[i_map][j_map]) {
+                    const currTile = this.tiles[i_map][j_map];
+                    filePath = "/static/img/assets/terrain/" + getAssetDir(currTile) + "/" + currTile + ".png";
+                } else {
+                    // Out-of bounds
+                    filePath = "/static/img/assets/terrain/Water/Water.1.1.png";
+                }
+                const img = this.terrainAssets[filePath];
+                if (img) {
+                    this.ctx.drawImage(img, x_screen * this.tileSize, y_screen * this.tileSize, this.tileSize, this.tileSize);
+                } else {
+                    console.error("drawTiles(): image does not exist")
+                }
+            }
+        }
+    }
+
+
+    isValidPosition(y, x) {
+        return y >= 0 && y < this.height
+            && x >= 0 && x < this.width;
+    }
+
+    getTile(y, x) {
+        if (this.isValidPosition(y, x)) {
+            return this.tiles[y][x];
+        } else {
+            console.error("gameTerrainMap : Invalid position: (y:", y, ", x:", x, ")");
+            return null;
+        }
     }
 
     toJSON() {
