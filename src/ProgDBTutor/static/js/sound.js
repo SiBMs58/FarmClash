982f1c025238c873b792fbb37e132aa599c3f9d0
camilledevuyst <devuyst.camille@gmail.com>
2024-04-25 19:27:05 +0200

    update project settings buttons

--- src/ProgDBTutor/static/js/sound.js
+++ src/ProgDBTutor/static/js/sound.js
@@ -15,7 +15,7 @@
     addSoundButton(imageElement, soundSrc, volume = 1) {
        const sound = new Audio(soundSrc);
        sound.volume = volume;
-       // sound.crossOrigin = "anonymous";
+       // music.crossOrigin = "anonymous";
 
         const soundButton = {
             imageElement: imageElement,
@@ -83,7 +83,7 @@
 
 
     mute() {
-    const muteSound = new Audio('./sound/discord-notification.mp3');
+    const muteSound = new Audio('./music/discord-notification.mp3');
     muteSound.play();
 
     if (!this.muted) {
@@ -100,7 +100,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const soundManager = new GameSoundManager();
 
 /*
-    const soundButton1 = document.querySelector('.sound-image-button1');
+    const soundButton1 = document.querySelector('.music-image-button1');
     var audio = new Audio();
     var scripts = document.getElementsByTagName('script');
     var currentScript = scripts[scripts.length - 1];
@@ -113,23 +113,23 @@ document.addEventListener('DOMContentLoaded', function() {
 
      */
 
-    const soundButton1 = document.querySelector('.sound-image-button1');
-    soundManager.addSoundButton(soundButton1, "{{ url_for('static', filename='sounds/sound1.mp3') }}", 0.7);
+    const soundButton1 = document.querySelector('.music-image-button1');
+    soundManager.addSoundButton(soundButton1, "{{ url_for('static', filename='/music/discord-notification.mp3') }}", 0.7);
 
-    const soundButton2 = document.querySelector('.sound-image-button2');
-    soundManager.addSoundButton(soundButton2, './sound/discord-notification.mp3', 0.7);
+    const soundButton2 = document.querySelector('.music-image-button2');
+    soundManager.addSoundButton(soundButton2, "{{ url_for('static', filename='/music/discord-notification.mp3') }}", 0.7);
 
-    const soundButton3 = document.querySelector('.sound-image-button3');
-    soundManager.addSoundButton(soundButton3, './sound/discord-notification.mp3', 0.7);
+    const soundButton3 = document.querySelector('.music-image-button3');
+    soundManager.addSoundButton(soundButton3, "{{ url_for('static', filename='/music/discord-notification.mp3') }}", 0.7);
 
-    const soundButton4 = document.querySelector('.sound-image-button4');
-    soundManager.addSoundButton(soundButton4, './sound/discord-notification.mp3', 0.7);
+    const soundButton4 = document.querySelector('.music-image-button4');
+    soundManager.addSoundButton(soundButton4, "{{ url_for('static', filename='/music/discord-notification.mp3') }}", 0.7);
 
-    const soundButton5 = document.querySelector('.sound-image-button5');
-    soundManager.addSoundButton(soundButton5, './sound/discord-notification.mp3', 0.7);
+    const soundButton5 = document.querySelector('.music-image-button5');
+    soundManager.addSoundButton(soundButton5, "{{ url_for('static', filename='/music/sound1.mp3') }}", 0.7);
 
-     const soundButton6 = document.querySelector('.sound-image-button6');
-    soundManager.addSoundButton(soundButton6, './sound/discord-notification.mp3', 0.7);
+     const soundButton6 = document.querySelector('.music-image-button6');
+    soundManager.addSoundButton(soundButton6, "{{ url_for('static', filename='/music/sound1.mp3') }}", 0.7);
 
     // Adjust volume for buttons algemeen
     //soundManager.setVolume(0.1);
@@ -167,11 +167,11 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 
-// adjust every button with its own sound or all buttons with the same => one function but specify music in html
+// adjust every button with its own music or all buttons with the same => one function but specify music in html
 /*
 document.addEventListener('DOMContentLoaded', function() {
     // Create instances of SoundImageButton for each image button
-    const imageButtons = document.querySelectorAll('.sound-image-button');
+    const imageButtons = document.querySelectorAll('.music-image-button');
     imageButtons.forEach(button => {
         const soundSrc = button.dataset.soundSrc;
         const volume = parseFloat(button.dataset.volume) || 1; // Default volume is 1 if not specified
