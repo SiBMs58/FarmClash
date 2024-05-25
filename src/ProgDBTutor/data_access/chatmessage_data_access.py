ccec85b79acda6531030d1e469b43d95fe2a850d
Siebe Mees <siebe.mees@student.uantwerpen.be>
2024-05-25 17:12:52 +0200

    FEAT: concurrency lock to chatmessage data access

--- src/ProgDBTutor/data_access/chatmessage_data_access.py
+++ src/ProgDBTutor/data_access/chatmessage_data_access.py
@@ -1,7 +1,9 @@
 from models.chatmessage import ChatMessage
+from threading import Lock
 class ChatMessageDataAccess:
     def __init__(self, db_connection):
         self.db_connection = db_connection
+        self.lock = Lock()
 
     def add_message(self, message):
         """
@@ -9,15 +11,16 @@ class ChatMessageDataAccess:
         :param message: A message object
         :return: True if the message was added successfully, False otherwise
         """
-        cursor = self.db_connection.get_cursor()
-        if message.message_id is None:
-            cursor.execute(
-                "INSERT INTO chat_messages (sender, receiver, message_text, sent_at) VALUES (%s, %s, %s, %s)",
-                (message.sender, message.receiver, message.message, message.created_at))
-        else:
-            cursor.execute("INSERT INTO chat_messages (message_id, sender, receiver, message_text, sent_at) VALUES (%s, %s, %s, %s, %s)", (message.message_id, message.sender, message.receiver, message.message, message.created_at))
-        self.db_connection.conn.commit()
-        return True
+        with self.lock:
+            cursor = self.db_connection.get_cursor()
+            if message.message_id is None:
+                cursor.execute(
+                    "INSERT INTO chat_messages (sender, receiver, message_text, sent_at) VALUES (%s, %s, %s, %s)",
+                    (message.sender, message.receiver, message.message, message.created_at))
+            else:
+                cursor.execute("INSERT INTO chat_messages (message_id, sender, receiver, message_text, sent_at) VALUES (%s, %s, %s, %s, %s)", (message.message_id, message.sender, message.receiver, message.message, message.created_at))
+            self.db_connection.conn.commit()
+            return True
 
     def get_messages(self, sender, receiver):
         """
@@ -26,11 +29,12 @@ class ChatMessageDataAccess:
         :param receiver: The user object of the receiver
         :return: A list of messages
         """
-        cursor = self.db_connection.get_cursor()
-        cursor.execute("""
-            SELECT * FROM chat_messages
-            WHERE (sender = %s AND receiver = %s) OR (sender = %s AND receiver = %s)
-            ORDER BY sent_at ASC
-            """, (sender.username, receiver.username, receiver.username, sender.username))
-        messages = cursor.fetchall()
-        return [ChatMessage(result['sender'], result['receiver'], result['message_text'], result['message_id'], result['sent_at']) for result in messages]
+        with self.lock:
+            cursor = self.db_connection.get_cursor()
+            cursor.execute("""
+                SELECT * FROM chat_messages
+                WHERE (sender = %s AND receiver = %s) OR (sender = %s AND receiver = %s)
+                ORDER BY sent_at ASC
+                """, (sender.username, receiver.username, receiver.username, sender.username))
+            messages = cursor.fetchall()
+            return [ChatMessage(result['sender'], result['receiver'], result['message_text'], result['message_id'], result['sent_at']) for result in messages]
