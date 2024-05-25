5e9bdc4d461d77f24a587070beb795ecdc3056ca
Siebe Mees <siebe.mees@student.uantwerpen.be>
2024-05-25 17:13:32 +0200

    FEAT: concurrency lock to crops data access

--- src/ProgDBTutor/data_access/crops_data_access.py
+++ src/ProgDBTutor/data_access/crops_data_access.py
@@ -1,8 +1,10 @@
 from models.crops import Crop
+from threading import Lock
 
 class CropsDataAccess:
     def __init__(self, db_connection):
         self.db_connection = db_connection
+        self.lock = Lock()
 
     def get_crop(self, crop_id):
         """
@@ -10,13 +12,14 @@ class CropsDataAccess:
         :param crop_id: the id of the crop (primary key)
         :return: The crop object with the given id, if not found return None
         """
-        cursor = self.db_connection.get_cursor()
-        cursor.execute("SELECT * FROM crops WHERE crop_id = %s", (crop_id,))
-        result = cursor.fetchone()
-        if result:
-            return Crop(result['name'], result['growth_time'], result['sell_price'])
-        else:
-            return None
+        with self.lock:
+            cursor = self.db_connection.get_cursor()
+            cursor.execute("SELECT * FROM crops WHERE crop_id = %s", (crop_id,))
+            result = cursor.fetchone()
+            if result:
+                return Crop(result['name'], result['growth_time'], result['sell_price'])
+            else:
+                return None
 
     def get_crop_by_name(self, name):
         """
@@ -24,13 +27,14 @@ class CropsDataAccess:
         :param name: the name of the crop
         :return: The crop object with the given name, if not found return None
         """
-        cursor = self.db_connection.get_cursor()
-        cursor.execute("SELECT * FROM crops WHERE name = %s", (name,))
-        result = cursor.fetchone()
-        if result:
-            return Crop(result['name'], result['growth_time'], result['sell_price'])
-        else:
-            return None
+        with self.lock:
+            cursor = self.db_connection.get_cursor()
+            cursor.execute("SELECT * FROM crops WHERE name = %s", (name,))
+            result = cursor.fetchone()
+            if result:
+                return Crop(result['name'], result['growth_time'], result['sell_price'])
+            else:
+                return None
 
     def get_id_from_name(self, name):
         """
@@ -38,13 +42,14 @@ class CropsDataAccess:
         :param name: the name of the crop
         :return: The crop id with the given name, if not found return None
         """
-        cursor = self.db_connection.get_cursor()
-        cursor.execute("SELECT crop_id FROM crops WHERE name = %s", (name,))
-        result = cursor.fetchone()
-        if result:
-            return result['crop_id']
-        else:
-            return None
+        with self.lock:
+            cursor = self.db_connection.get_cursor()
+            cursor.execute("SELECT crop_id FROM crops WHERE name = %s", (name,))
+            result = cursor.fetchone()
+            if result:
+                return result['crop_id']
+            else:
+                return None
 
     def add_crop(self, crop):
         """
@@ -52,19 +57,20 @@ class CropsDataAccess:
         :param crop: A crop object
         :return: True if the crop was added successfully, False otherwise
         """
-        cursor = self.db_connection.get_cursor()
-        # Check if the crop with the given name already exists
-        cursor.execute("SELECT * FROM crops WHERE name = %s", (crop.name,))
-        existing_crop = cursor.fetchone()
-        if existing_crop:
-            return False  # Crop with the given name already exists
-        else:
-            # Insert the new crop into the database
-            cursor.execute("""
-                INSERT INTO crops (name, growth_time, sell_price)
-                VALUES (%s, %s, %s)
-                RETURNING crop_id;
-            """, (crop.name, crop.growth_time, crop.sell_price))
-            crop_id = cursor.fetchone()[0]
-            self.db_connection.conn.commit()
-            return crop_id is not None
+        with self.lock:
+            cursor = self.db_connection.get_cursor()
+            # Check if the crop with the given name already exists
+            cursor.execute("SELECT * FROM crops WHERE name = %s", (crop.name,))
+            existing_crop = cursor.fetchone()
+            if existing_crop:
+                return False  # Crop with the given name already exists
+            else:
+                # Insert the new crop into the database
+                cursor.execute("""
+                    INSERT INTO crops (name, growth_time, sell_price)
+                    VALUES (%s, %s, %s)
+                    RETURNING crop_id;
+                """, (crop.name, crop.growth_time, crop.sell_price))
+                crop_id = cursor.fetchone()[0]
+                self.db_connection.conn.commit()
+                return crop_id is not None
