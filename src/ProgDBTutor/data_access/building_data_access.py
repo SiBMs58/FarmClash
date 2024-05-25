f66110aec21290ff508d4aa7ef612bf07c491a39
Siebe Mees <siebe.mees@student.uantwerpen.be>
2024-05-25 17:12:10 +0200

    FEAT: concurrency lock to building data access

--- src/ProgDBTutor/data_access/building_data_access.py
+++ src/ProgDBTutor/data_access/building_data_access.py
@@ -1,8 +1,10 @@
 from models.building import Building
+from threading import Lock
 
 class BuildingDataAccess:
     def __init__(self, db_connection):
         self.db_connection = db_connection
+        self.lock = Lock()
 
     def get_building(self, building_id):
         """
@@ -10,15 +12,16 @@ class BuildingDataAccess:
         :param building_id: the id of the building aka the primary key
         :return: The building object with the given id, if not found return None
         """
-        cursor = self.db_connection.get_cursor()
-        cursor.execute("SELECT * FROM buildings WHERE building_id = %s", (building_id,))
-        result = cursor.fetchone()
-        if result:
-            return Building(result['building_id'], result['username_owner'], result['farm_id'],
-                            result['building_type'], result['level'], result['x'], result['y'],
-                            result['tile_rel_locations'], result['created_at'], result['augment_level'])
-        else:
-            return None
+        with self.lock:
+            cursor = self.db_connection.get_cursor()
+            cursor.execute("SELECT * FROM buildings WHERE building_id = %s", (building_id,))
+            result = cursor.fetchone()
+            if result:
+                return Building(result['building_id'], result['username_owner'], result['farm_id'],
+                                result['building_type'], result['level'], result['x'], result['y'],
+                                result['tile_rel_locations'], result['created_at'], result['augment_level'])
+            else:
+                return None
 
     def get_buildings_by_username_owner(self, username_owner):
         """
@@ -26,38 +29,19 @@ class BuildingDataAccess:
         :param username_owner: the username of the owner
         :return: A list of Building objects belonging to the given username_owner, an empty list if none found
         """
-        cursor = self.db_connection.get_cursor()
-        cursor.execute("SELECT * FROM buildings WHERE username_owner = %s", (username_owner,))
-        results = cursor.fetchall()
+        with self.lock:
+            cursor = self.db_connection.get_cursor()
+            cursor.execute("SELECT * FROM buildings WHERE username_owner = %s", (username_owner,))
+            results = cursor.fetchall()
 
-        buildings = []
-        for result in results:
-            building = Building(result['building_id'], result['username_owner'], result['building_type'],
-                                result['level'], result['x'], result['y'], result['tile_rel_locations'],
-                                result['created_at'], result['augment_level'])
-            buildings.append(building)
+            buildings = []
+            for result in results:
+                building = Building(result['building_id'], result['username_owner'], result['building_type'],
+                                    result['level'], result['x'], result['y'], result['tile_rel_locations'],
+                                    result['created_at'], result['augment_level'])
+                buildings.append(building)
 
-        return buildings
-
-    def get_buildings_by_username_and_type(self, username_owner, building_type):
-        """
-        Fetches all buildings belonging to the given username_owner and building_type from the database
-        :param username_owner: the username of the owner
-        :param building_type: the type of the building
-        :return: A list of Building objects belonging to the given username_owner and building_type, an empty list if none found
-        """
-        cursor = self.db_connection.get_cursor()
-        cursor.execute("SELECT * FROM buildings WHERE username_owner = %s AND building_type = %s", (username_owner, building_type))
-        results = cursor.fetchall()
-
-        buildings = []
-        for result in results:
-            building = Building(result['building_id'], result['username_owner'], result['building_type'],
-                                result['level'], result['x'], result['y'], result['tile_rel_locations'],
-                                result['created_at'], result['augment_level'])
-            buildings.append(building)
-
-        return buildings
+            return buildings
 
     def get_buildings_by_username_and_type(self, username_owner, building_type):
         """
@@ -66,19 +50,42 @@ class BuildingDataAccess:
         :param building_type: the type of the building
         :return: A list of Building objects belonging to the given username_owner and building_type, an empty list if none found
         """
-        cursor = self.db_connection.get_cursor()
-        cursor.execute("SELECT * FROM buildings WHERE username_owner = %s AND building_type = %s",
-                       (username_owner, building_type))
-        results = cursor.fetchall()
+        with self.lock:
+            cursor = self.db_connection.get_cursor()
+            cursor.execute("SELECT * FROM buildings WHERE username_owner = %s AND building_type = %s",
+                           (username_owner, building_type))
+            results = cursor.fetchall()
 
-        buildings = []
-        for result in results:
-            building = Building(result['building_id'], result['username_owner'], result['building_type'],
-                                result['level'], result['x'], result['y'], result['tile_rel_locations'],
-                                result['created_at'], result['augment_level'])
-            buildings.append(building)
+            buildings = []
+            for result in results:
+                building = Building(result['building_id'], result['username_owner'], result['building_type'],
+                                    result['level'], result['x'], result['y'], result['tile_rel_locations'],
+                                    result['created_at'], result['augment_level'])
+                buildings.append(building)
 
-        return buildings
+            return buildings
+
+    def get_buildings_by_username_and_type(self, username_owner, building_type):
+        """
+        Fetches all buildings belonging to the given username_owner and building_type from the database
+        :param username_owner: the username of the owner
+        :param building_type: the type of the building
+        :return: A list of Building objects belonging to the given username_owner and building_type, an empty list if none found
+        """
+        with self.lock:
+            cursor = self.db_connection.get_cursor()
+            cursor.execute("SELECT * FROM buildings WHERE username_owner = %s AND building_type = %s",
+                           (username_owner, building_type))
+            results = cursor.fetchall()
+
+            buildings = []
+            for result in results:
+                building = Building(result['building_id'], result['username_owner'], result['building_type'],
+                                    result['level'], result['x'], result['y'], result['tile_rel_locations'],
+                                    result['created_at'], result['augment_level'])
+                buildings.append(building)
+
+            return buildings
 
     def add_building(self, building):
         """
@@ -86,39 +93,38 @@ class BuildingDataAccess:
         :param building: A Building object
         :return: True if the building was added/updated successfully, False otherwise
         """
-        cursor = self.db_connection.get_cursor()
-        try:
-            # Check if the building already exists for the given username and building_id
-            cursor.execute("SELECT * FROM buildings WHERE username_owner = %s AND building_id = %s",
-                           (building.username_owner, building.building_id))
-            existing_building = cursor.fetchone()
-            if existing_building:
-                # Update the existing building entry
-                cursor.execute("""
-                    UPDATE buildings
-                    SET building_type = %s, level = %s, x = %s, y = %s, 
-                    tile_rel_locations = %s, created_at = %s, augment_level = %s
-                    WHERE username_owner = %s AND building_id = %s;
-                """, (building.building_type, building.level,building.x, building.y,
-                      building.tile_rel_locations, building.created_at, building.augment_level,
-                      building.username_owner, building.building_id, ))
-            else:
-                # Insert a new building entry
-                cursor.execute("""
-                    INSERT INTO buildings (building_id, username_owner, building_type, level, x, y, 
-                    tile_rel_locations, created_at, augment_level)
-                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
-                    ON CONFLICT (username_owner, building_id) DO UPDATE 
-                    SET building_type = EXCLUDED.building_type, level = EXCLUDED.level,
-                        x = EXCLUDED.x, y = EXCLUDED.y, tile_rel_locations = EXCLUDED.tile_rel_locations,
-                        created_at = EXCLUDED.created_at;
-                """, (building.building_id, building.username_owner, building.building_type,
-                      building.level, building.x, building.y, building.tile_rel_locations, building.created_at, building.augment_level))
-            self.db_connection.conn.commit()
-            return True
-        except Exception as e:
-            # Handle exceptions
-            print("Error:", e)
-            return False
-
-
+        with self.lock:
+            cursor = self.db_connection.get_cursor()
+            try:
+                # Check if the building already exists for the given username and building_id
+                cursor.execute("SELECT * FROM buildings WHERE username_owner = %s AND building_id = %s",
+                               (building.username_owner, building.building_id))
+                existing_building = cursor.fetchone()
+                if existing_building:
+                    # Update the existing building entry
+                    cursor.execute("""
+                        UPDATE buildings
+                        SET building_type = %s, level = %s, x = %s, y = %s, 
+                        tile_rel_locations = %s, created_at = %s, augment_level = %s
+                        WHERE username_owner = %s AND building_id = %s;
+                    """, (building.building_type, building.level,building.x, building.y,
+                          building.tile_rel_locations, building.created_at, building.augment_level,
+                          building.username_owner, building.building_id, ))
+                else:
+                    # Insert a new building entry
+                    cursor.execute("""
+                        INSERT INTO buildings (building_id, username_owner, building_type, level, x, y, 
+                        tile_rel_locations, created_at, augment_level)
+                        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+                        ON CONFLICT (username_owner, building_id) DO UPDATE 
+                        SET building_type = EXCLUDED.building_type, level = EXCLUDED.level,
+                            x = EXCLUDED.x, y = EXCLUDED.y, tile_rel_locations = EXCLUDED.tile_rel_locations,
+                            created_at = EXCLUDED.created_at;
+                    """, (building.building_id, building.username_owner, building.building_type,
+                          building.level, building.x, building.y, building.tile_rel_locations, building.created_at, building.augment_level))
+                self.db_connection.conn.commit()
+                return True
+            except Exception as e:
+                # Handle exceptions
+                print("Error:", e)
+                return False
