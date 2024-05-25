e1e4572563ec1951f973681518631e6532ac2c2b
Siebe Mees <siebe.mees@student.uantwerpen.be>
2024-05-25 17:18:57 +0200

    FEAT: concurrency lock to user data access

--- src/ProgDBTutor/data_access/user_data_access.py
+++ src/ProgDBTutor/data_access/user_data_access.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from threading import Lock
 
 from models.user import User
 from werkzeug.security import generate_password_hash
@@ -8,6 +9,7 @@ class UserDataAccess:
 
     def __init__(self, db_connection):
         self.db_connection = db_connection
+        self.lock = Lock()
 
 
     def add_user(self, user):
@@ -16,14 +18,15 @@ class UserDataAccess:
         :param user: User object with user data.
         :return: True if user is added successfully, False otherwise.
         """
-        if self.get_user(user.username):
-            return False
+        with self.lock:
+            if self.get_user(user.username):
+                return False
 
-        cursor = self.db_connection.get_cursor()
-        cursor.execute("INSERT INTO users (username, password, email, created_at) VALUES (%s, %s, %s, %s)",
-                       (user.username, generate_password_hash(user.password), user.email, user.created_at))
-        self.db_connection.conn.commit()
-        return True
+            cursor = self.db_connection.get_cursor()
+            cursor.execute("INSERT INTO users (username, password, email, created_at) VALUES (%s, %s, %s, %s)",
+                           (user.username, generate_password_hash(user.password), user.email, user.created_at))
+            self.db_connection.conn.commit()
+            return True
 
     def get_user(self, username):
         """
@@ -31,22 +34,24 @@ class UserDataAccess:
         :param username: The username of the user to fetch, aka the primary key.
         :return: User object with user data or None if user is not found.
         """
-        cursor = self.db_connection.get_cursor()
-        cursor.execute("SELECT * FROM users WHERE username = %s", (username,))
-        result = cursor.fetchone()
-        if result:
-           return User(result['username'], result['password'], result['email'], result['created_at'], result['last_gift'])
-        else:
-           return None
+        with self.lock:
+            cursor = self.db_connection.get_cursor()
+            cursor.execute("SELECT * FROM users WHERE username = %s", (username,))
+            result = cursor.fetchone()
+            if result:
+               return User(result['username'], result['password'], result['email'], result['created_at'], result['last_gift'])
+            else:
+               return None
 
     def update_last_gift(self, username):
         """
         Updates the last_gift field of the user with the given username.
         :param username: The username of the user to update.
         """
-        cursor = self.db_connection.get_cursor()
-        cursor.execute("UPDATE users SET last_gift = %s WHERE username = %s", (datetime.now(), username))
-        self.db_connection.conn.commit()
+        with self.lock:
+            cursor = self.db_connection.get_cursor()
+            cursor.execute("UPDATE users SET last_gift = %s WHERE username = %s", (datetime.now(), username))
+            self.db_connection.conn.commit()
 
 
     def get_all_users(self):
@@ -54,10 +59,11 @@ class UserDataAccess:
         Fetches all users from the database.
         :return: List of User objects with user data.
         """
-        cursor = self.db_connection.get_cursor()
-        cursor.execute("SELECT * FROM users")
-        results = cursor.fetchall()
-        return [User(result['username'], result['password'], result['email'], result['created_at'], result['last_gift']) for result in results]
+        with self.lock:
+            cursor = self.db_connection.get_cursor()
+            cursor.execute("SELECT * FROM users")
+            results = cursor.fetchall()
+            return [User(result['username'], result['password'], result['email'], result['created_at'], result['last_gift']) for result in results]
 
     def search_users(self, query):
         """
@@ -65,9 +71,10 @@ class UserDataAccess:
         :param query: The search query string.
         :return: List of User objects for users whose usernames match the query.
         """
-        cursor = self.db_connection.get_cursor()
-        like_query = f"%{query}%"
-        cursor.execute("SELECT * FROM users WHERE username LIKE %s", (like_query,))
-        results = cursor.fetchall()
-        return [User(result['username'], result['password'], result['email'], result['created_at'], result['last_gift']) for result in
-                results]
+        with self.lock:
+            cursor = self.db_connection.get_cursor()
+            like_query = f"%{query}%"
+            cursor.execute("SELECT * FROM users WHERE username LIKE %s", (like_query,))
+            results = cursor.fetchall()
+            return [User(result['username'], result['password'], result['email'], result['created_at'], result['last_gift']) for result in
+                    results]
