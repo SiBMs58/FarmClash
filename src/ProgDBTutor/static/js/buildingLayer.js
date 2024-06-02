ae689d14c24ce9458695732bc0f8c863dd647637
Faisal <faisal.ettarrahi@hotmail.com>
2024-06-03 01:17:12 +0200

    default building map changes

--- src/ProgDBTutor/static/js/buildingLayer.js
+++ src/ProgDBTutor/static/js/buildingLayer.js
@@ -92,7 +92,7 @@ export class BuildingMap extends BaseMap {
     async initialize() {
         await this.fetchBuildingAssetList();
         await this.fetchRelativeLocations();
-        //await this.fetchBuildingMapData();
+        await this.fetchBuildingMapData();
         this.tiles = this.generateBuildingTileMap();
         await new Promise((resolve) => this.preloadBuildingAssets(resolve));
         // Safe to call stuff here
